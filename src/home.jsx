d268162b95f75b3b67aa62957af3dfcd07e527aa
deadbeefiv <kook.signvrse@gmail.com>
2024-09-19 13:46:00 +0300

    added NCA roads layer

--- src/home.jsx
+++ src/home.jsx
@@ -32,13 +32,19 @@ export function Home() {
                 <MapContainer center={position} zoom={13} minZoom={7}>
                     <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png"
+                        url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
                     />
-                    <Marker position={position}> 
+
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        opacity={0.3}
+                    />
+                    {/* <Marker position={position}> 
                         <Popup> {element.label} </Popup>
                     </Marker>
                     <AirportMarker airports={airports}/>
-                    <AirspaceBuffer airspace={airspace}/> 
+                    <AirspaceBuffer airspace={airspace}/>  */}
                 </MapContainer>
           </div>
       </div>
